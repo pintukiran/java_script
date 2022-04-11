a8020bf3ede44b321436490cb5b2c9ddf2daeb12
pintukiran <kiranpintu633@gmail.com>
2022-04-11 07:17:06 +0530

    Data_types

---
+++ Datatypes/num2.js
@@ -0,0 +1,4 @@
+let data=8
+let user="kiran";
+let num2=275678912332
+console.log(num2)
---
+++ Datatypes/number.js
@@ -0,0 +1,8 @@
+let data=8;
+let user="kiran";
+
+let num=7.8
+console.log(typeof data);
+console.log(typeof num);
+console.log(typeof user);
+//typeof is a keyword to know which variable stores either number or string 
