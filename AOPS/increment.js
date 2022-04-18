290759c55025534b4a43fd2efb04c60ba6b5fdfb
pintukiran <kiranpintu633@gmail.com>
2022-04-18 23:16:20 +0530

    Arthmetic_operators

---
+++ AOPS/increment.js
@@ -0,0 +1,10 @@
+/////////post increment///////////////
+let x=2
+ let y=x++;
+console.log(y,x);
+
+console.log("////////////////////////////");
+///////////pre increment//////////////
+let a=4
+let b=++a
+console.log(b,a);
