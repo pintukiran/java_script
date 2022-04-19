2478a6d2a4a81f1fb6a47307ca2d66af607e5ebb
pintukiran <kiranpintu633@gmail.com>
2022-04-19 22:31:44 +0530

    ROPS & LOPS

---
+++ logical_OPs/LOPS1.js
@@ -0,0 +1,14 @@
+let x=7,y=8,z=9
+let result=x<y&&x<z
+console.log(result);
+
+console.log("/////////////////////////");
+
+let a=7,b=8,c=5
+let result1=a<b||a<c
+let n=!result1
+console.log(!result1,n);
+
+console.log("/////////////////////////");
+
+
