1f42de39ada80519152caa039abe69a37f8ebffe
pintukiran <kiranpintu633@gmail.com>
2022-04-20 07:18:35 +0530

    if_else statements

---
+++ Conditional_statements/CS.js
@@ -0,0 +1,26 @@
+let x=6;
+let y=4;
+if(x>y){
+    console.log("x is even");
+}
+else{
+    console.log("y is odd");
+}
+
+console.log("//////////////////////////////////////////");
+
+let a=6;
+let b=4;
+let c=7;
+if(a>b && a>c){
+    console.log("condition1");
+}
+else if(c>a || a<b){
+    console.log("condition2");
+}
+else{
+    console.log("condition3");
+}
+
+console.log("//////////////////////////////////////////");
+
