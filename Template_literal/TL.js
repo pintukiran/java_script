6e9dfd529fb6dfdc1e00f45c99c8edc5eec65cb3
pintukiran <kiranpintu633@gmail.com>
2022-04-22 22:33:11 +0530

    Template_literal

---
+++ Template_literal/TL.js
@@ -0,0 +1,13 @@
+let num1=9
+let num2=4
+let result=num1+num2
+//console.log("The addition of" +num1+  "and"+ num2 +"is"   +  result);
+
+console.log(`The addition of ${num1}  and  ${num2} is ${result}`);
+
+
+console.log("////////////////////////////");
+
+console.log("pintu \n kiran");//normal code
+console.log(`my name is 
+kiran`);//Template formate code
