af2fb4a1a04862910a36d9fe890e3dfb609153cc
KavithaHT <kavithaht910@gmail.com>
2022-04-25 22:49:30 +0530

    whileVSforloop and object

---
+++ whileVsforloop/forloop.js
@@ -0,0 +1,14 @@
+//if we have to print 1 to 100 we can use for loop
+//we can print even number and odd number in b/w 1 to 100
+for(let i=0;i<=10;i++){
+    if(i%3===0)
+    console.log(i);
+}
+console.log("/////////////////////////////////");
+
+for(let i=0;i<=10;i++){
+    if(i%2===0)
+    console.log(i);
+}
+
+
