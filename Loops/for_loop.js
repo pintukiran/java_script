ded8256fb67118b231419ea0dc19e5305d2dfce9
pintukiran <pintukiran633@gmail.com>
2022-04-25 06:53:10 +0530

    Loops1

---
+++ Loops/for_loop.js
@@ -0,0 +1,10 @@
+for(let i=1;i<=5;i++){
+    console.log("Hi",i);
+}
+
+console.log("////////////////////////////////////");
+for(let i=1;i<=5;i++){
+    console.log("Hi",i);
+    for(let j=1;j<=5;j++)
+    console.log("Hello",j);
+}
