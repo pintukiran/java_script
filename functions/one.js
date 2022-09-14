398c778d20805057c69c51e8a404c0d7e73ec3ec
KavithaHT <kavithaht910@gmail.com>
2022-09-14 08:37:07 +0530

    function2

---
+++ functions/one.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    console.log(a+b);
+}
+add(10,30)
+console.log("//////////////////////////////////////////////////"    );
+
+function wish(a,b,c=10){
+console.log(a+b+c);
+}
+wish(10,203,)
+console.log("/////////////////////////////////////////////");
+function add1(a,b){
+    return a+b;
+}
+var r1=add1(10,020)
+console.log(r1);
+
