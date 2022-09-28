1b783310eb3865195bcd94968bb376863db44363
KavithaHT <kavithaht910@gmail.com>
2022-09-28 18:40:26 +0530

    okay

---
+++ OOP/one.js
@@ -0,0 +1,11 @@
+//Object oriented programming paradigm based on the concept of object
+//Object:- Object are like real life entities they have their own properties and methodes
+class emp{
+    ID=101
+    Name="Kiran"
+    sal=45000
+
+}let e1=new emp()
+console.log(e1);
+console.log("/////////////////////////////////////////////////////////////////");
+
